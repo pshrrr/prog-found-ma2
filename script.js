70a7eb5d8b10312dff488e514b289fdfc9acc17a
pshrrr <59803973+pshrrr@users.noreply.github.com>
2020-03-12 12:13:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+// Question 1
+
+var petArray = ["cat", "cow", "dog"];
+
+// Question 2
+
+console.log(petArray[1]);
+
+// Question 3
+
+petArray.push("sheep");
+
+// Question 4
+
+console.log(petArray.length);
+
+// Question 5
+
+var catObject = {
+  name: "Coolio",
+  colour: "red",
+  age: 11
+};
+
+// Question 6
+
+console.log(catObject.age);
+
+// Question 7
+
+var catArray = [
+  {
+    name: "Coolio",
+    colour: "red",
+    age: 11
+  }
+];
+
+// Question 8
+
+for (var i = 0; i < catArray.length; i++) {
+  console.log(catArray[i]);
+}
+
+// Question 9
+
+function logToConsole(message) {
+  console.log("Hello My fellow students");
+}
+
+// Question 10
+
+function logToConsole(message) {
+  console.log(message);
+}
+
+logToConsole("donkey");
